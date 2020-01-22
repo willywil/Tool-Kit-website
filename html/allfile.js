b70815a30b75c362c10c21518f2f8a66b176ae20
willywil <jonwilhelmjr@hotmail.com>
2020-01-22 11:39:12 -0600

    Added blog and folder/file structure

--- html/allfile.js
+++ html/allfile.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
     
     
     $("#menu").append("<a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>&times;</a>");
-    $("#menu").append("<li id='mna'><a href='aerospace.html'>Aerospace</a></li>");
-	$("#menu").append("<li id='mna'><a href='basics.html'>Basics</a></li>");
-	$("#menu").append("<li id='mna'><a href='biological.html'>Biological</a></li>");
-	$("#menu").append("<li id='mna'><a href='chemical.html'>Chemical</a></li>");
-	$("#menu").append("<li id='mna'><a href='electrical.html'>Electrical</a></li>");
-    $("#menu").append("<li id='mna'><a href='mechanical.html'>Mechanical</a></li>");
+    $("#menu").append("<li id='mna'><a href='../subjects/aerospace.html'>Aerospace</a></li>");
+	$("#menu").append("<li id='mna'><a href='../subjects/basics.html'>Basics</a></li>");
+	$("#menu").append("<li id='mna'><a href='../subjects/biological.html'>Biological</a></li>");
+	$("#menu").append("<li id='mna'><a href='../subjects/chemical.html'>Chemical</a></li>");
+	$("#menu").append("<li id='mna'><a href='../subjects/electrical.html'>Electrical</a></li>");
+    $("#menu").append("<li id='mna'><a href='../subjects/mechanical.html'>Mechanical</a></li>");
     
    
     $("#at_the_bottom").append('<li><a href="#" class="up">Go UP</a></li>');
